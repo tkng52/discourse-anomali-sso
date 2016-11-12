11cccdd4bc15026ab1baf9c8fdfb370f2c85e085
Tze Kang Ng <tn52@duke.edu>
2016-11-11 18:42:07 -0800

    first commit

---
+++ controllers/anomali_controller.rb
@@ -0,0 +1,133 @@
+class ::AnomaliController < ::ApplicationController
+
+  skip_before_filter :redirect_to_login_if_required
+  skip_before_filter :preload_json, :check_xhr, only: ['sso_threatstream', 'sso_reports', 'sso_login']
+
+
+  def sso_threatstream
+    destination_url = cookies[:destination_url] || session[:destination_url]
+    return_path = params[:return_path] || path('/')
+
+    if destination_url && return_path == path('/')
+      uri = URI::parse(destination_url)
+      return_path = "#{uri.path}#{uri.query ? "?" << uri.query : ""}"
+    end
+
+    session.delete(:destination_url)
+    cookies.delete(:destination_url)
+
+    sso = DiscourseSingleSignOn.generate_sso(return_path)
+    if SiteSetting.verbose_sso_logging
+      Rails.logger.warn("Verbose SSO log: Started SSO process\n\n#{sso.diagnostics}")
+    end
+
+    base = SiteSetting.threatstream_sso_url
+    "#{base}#{base.include?('?') ? '&' : '?'}#{sso.payload}"
+    redirect_to sso.to_url
+  end
+
+  def sso_reports
+    destination_url = cookies[:destination_url] || session[:destination_url]
+    return_path = params[:return_path] || path('/')
+
+    if destination_url && return_path == path('/')
+      uri = URI::parse(destination_url)
+      return_path = "#{uri.path}#{uri.query ? "?" << uri.query : ""}"
+    end
+
+    session.delete(:destination_url)
+    cookies.delete(:destination_url)
+
+    sso = DiscourseSingleSignOn.generate_sso(return_path)
+    if SiteSetting.verbose_sso_logging
+      Rails.logger.warn("Verbose SSO log: Started SSO process\n\n#{sso.diagnostics}")
+    end
+
+    base = SiteSetting.reports_sso_url
+    redirect_to "#{base}#{base.include?('?') ? '&' : '?'}#{sso.payload}"
+  end
+
+  def sso_login
+    # unless SiteSetting.enable_sso
+    #   return render(nothing: true, status: 404)
+    # end
+
+    sso = DiscourseSingleSignOn.parse(request.query_string)
+    if !sso.nonce_valid?
+      if SiteSetting.verbose_sso_logging
+        Rails.logger.warn("Verbose SSO log: Nonce has already expired\n\n#{sso.diagnostics}")
+      end
+      return render(text: I18n.t("sso.timeout_expired"), status: 419)
+    end
+
+    if ScreenedIpAddress.should_block?(request.remote_ip)
+      if SiteSetting.verbose_sso_logging
+        Rails.logger.warn("Verbose SSO log: IP address is blocked #{request.remote_ip}\n\n#{sso.diagnostics}")
+      end
+      return render(text: I18n.t("sso.unknown_error"), status: 500)
+    end
+
+    return_path = sso.return_path
+    sso.expire_nonce!
+
+    begin
+      if user = sso.lookup_or_create_user(request.remote_ip)
+
+        if SiteSetting.must_approve_users? && !user.approved?
+          if SiteSetting.sso_not_approved_url.present?
+            redirect_to SiteSetting.sso_not_approved_url
+          else
+            render text: I18n.t("sso.account_not_approved"), status: 403
+          end
+          return
+        elsif !user.active?
+          activation = UserActivator.new(user, request, session, cookies)
+          activation.finish
+          session["user_created_message"] = activation.message
+          redirect_to users_account_created_path and return
+        else
+          if SiteSetting.verbose_sso_logging
+            Rails.logger.warn("Verbose SSO log: User was logged on #{user.username}\n\n#{sso.diagnostics}")
+          end
+          log_on_user user
+        end
+
+        # If it's not a relative URL check the host
+        if return_path !~ /^\/[^\/]/
+          begin
+            uri = URI(return_path)
+            return_path = path("/") unless uri.host == Discourse.current_hostname
+          rescue
+            return_path = path("/")
+          end
+        end
+
+        redirect_to return_path
+      else
+        render text: I18n.t("sso.not_found"), status: 500
+      end
+    rescue ActiveRecord::RecordInvalid => e
+      if SiteSetting.verbose_sso_logging
+        Rails.logger.warn(<<-EOF)
+          Verbose SSO log: Record was invalid: #{e.record.class.name} #{e.record.id}\n
+          #{e.record.errors.to_h}\n
+          \n
+          #{sso.diagnostics}
+        EOF
+      end
+      render text: I18n.t("sso.unknown_error"), status: 500
+    rescue => e
+      message = "Failed to create or lookup user: #{e}."
+      message << "\n\n" << "-" * 100 << "\n\n"
+      message << sso.diagnostics
+      message << "\n\n" << "-" * 100 << "\n\n"
+      message << e.backtrace.join("\n")
+
+      Rails.logger.error(message)
+
+      render text: I18n.t("sso.unknown_error"), status: 500
+    end
+  end
+
+
+end
---
+++ plugin.rb
@@ -0,0 +1,92 @@
+# name: discourse-anomali-sso
+# about: Anomali SSO Plugin
+# version: 0.1
+# authors: TK
+
+require_dependency 'auth/oauth2_authenticator.rb'
+
+enabled_site_setting :threatstream_enabled
+enabled_site_setting :reports_enabled
+
+after_initialize do
+  load File.expand_path("../controllers/anomali_controller.rb", __FILE__)
+
+  Discourse::Application.routes.prepend do
+    get 'anomali/sso_threatstream' => 'anomali#sso_threatstream'
+    get 'anomali/sso_reports' => 'anomali#sso_reports'
+    get 'anomali/sso_login' => 'anomali#sso_login'
+  end
+
+end
+
+class ::OmniAuth::Strategies::Oauth2Basic < ::OmniAuth::Strategies::OAuth2
+  option :name, "oauth2_basic"
+  info do
+    {
+      id: access_token['id']
+    }
+  end
+end
+
+
+############### Theatstream ###############
+
+class OAuth2BasicAuthenticator < ::Auth::OAuth2Authenticator
+  def register_middleware(omniauth)
+    omniauth.provider :oauth2_basic,
+                      name: 'threatstream_basic',
+                      setup: lambda {|env|
+                        opts = env['omniauth.strategy'].options
+                      }
+  end
+
+  def after_authenticate(auth)
+  end
+
+end
+
+auth_provider title_setting: "threatstream_button_title",
+              enabled_setting: "threatstream_enabled",
+              authenticator: OAuth2BasicAuthenticator.new('threatstream_basic'),
+              message: "OAuth2",
+              custom_url: "https://devdiscourse01.threatstream.com/anomali/sso_threatstream?return_path=%2F",
+              # custom_url: "http://localhost:4000/anomali/sso_threatstream?return_path=%2F",
+              full_screen_login: true
+
+register_css <<CSS
+  button.btn-social.threatstream_basic {
+    background-color: #57C4BF;
+  }
+CSS
+
+
+
+ ############### Reports ###############
+
+class ReportsBasicAuthenticator < ::Auth::OAuth2Authenticator
+  def register_middleware(omniauth)
+    omniauth.provider :oauth2_basic,
+                      name: 'reports_basic',
+                      setup: lambda {|env|
+                        opts = env['omniauth.strategy'].options
+                      }
+  end
+
+  def after_authenticate(auth)
+  end
+
+end
+
+auth_provider title_setting: "reports_button_title",
+              enabled_setting: "reports_enabled",
+              authenticator: OAuth2BasicAuthenticator.new('reports_basic'),
+              message: "OAuth2",
+              custom_url: "https://devdiscourse01.threatstream.com/anomali/sso_reports?return_path=%2F",
+              # custom_url: "http://localhost:4000/anomali/sso_reports?return_path=%2F",
+              full_screen_login: true
+
+register_css <<CSS
+  button.btn-social.reports_basic {
+    background-color: #F79420;
+  }
+CSS
