9a95c91aaf8696607fd38310c60d7d49c1e4cad8
Tze Kang Ng <tn52@duke.edu>
2016-11-14 11:04:40 -0800

    revert to original redirect url

--- controllers/anomali_controller.rb
+++ controllers/anomali_controller.rb
@@ -22,8 +22,8 @@ class ::AnomaliController < ::ApplicationController
     end
 
     base = SiteSetting.threatstream_sso_url
-    redirect_to "#{base}#{base.include?('?') ? '&' : '?'}#{sso.payload}"
-    # redirect_to sso.to_url
+    # redirect_to "#{base}#{base.include?('?') ? '&' : '?'}#{sso.payload}"
+    redirect_to sso.to_url
   end
 
   def sso_reports
@@ -44,7 +44,8 @@ class ::AnomaliController < ::ApplicationController
     end
 
     base = SiteSetting.reports_sso_url
-    redirect_to "#{base}#{base.include?('?') ? '&' : '?'}#{sso.payload}"
+    # redirect_to "#{base}#{base.include?('?') ? '&' : '?'}#{sso.payload}"
+    redirect_to sso.to_url
   end
 
   def sso_login
